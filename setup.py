ae9a68bd8ee1420e433965afac4bb99eed5e0899
Italo Maia <italo.maia@gmail.com>
2011-09-20 21:49:55 -0300

    githubt url added to setup.py and py_modules (dont know if it works yet)

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     name="data-factory",
     author="Italo Maia",
     author_email="italo.maia@gmail.com",
-    url="",
+    url="https://github.com/italomaia/data-factory",
     keywords="data factory testing",
     description="Simple collection of data generating functions very useful for testing software.",
+    py_modules=["src.data_factory"],
 )
